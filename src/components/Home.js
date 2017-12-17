13b2d2a3346de4435edf5cc85ccad0814a6363ce
Eric Phillips <dephil29@hotmail.com>
2017-12-17 17:23:32 -0500

    it doesn't break when i click the button so that's cool

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
+import Form from './Form'
 
 //use this function if there is no id
-function generateId(){
-    return Math.random()
-        .toString(36)
-        .substr(2, 9);
-}
+// function generateId(){
+//     return Math.random()
+//         .toString(36)
+//         .substr(2, 9);
+// }
 
 class Home extends Component {
-    state = {
-        data: []
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [],
+        };
     }
 
     componentDidMount(){
@@ -19,19 +23,27 @@ class Home extends Component {
     }
 
     render(){
-        console.log(generateId());
         const {data} = this.state;
-        console.log(data);
         return (
             <div>
-                <h2>I am home!</h2>
-                <ul>
-                {data.map(prod => (
-                    <div key={prod.id}>
-                        <h3>{prod.name}</h3>
+                <h2>ACME Products</h2>
+                <div className="thing">
+                    <div className="list">
+                        <ul>
+                        {data.map(prod => (
+                            <div className="tables" key={prod.id}>
+                                <h3>Product Name: {prod.name}</h3>
+                                <h4>Product Description: {prod.description}</h4>
+                                <h4>Price: {prod.price}</h4>
+                                <h4>ID: {prod.id}</h4>
+                            </div>
+                        ))}
+                        </ul>
                     </div>
-                ))}
-                </ul>
+                    <div className="display">
+                        <Form />
+                    </div>
+                </div>
             </div>
         )
     }
